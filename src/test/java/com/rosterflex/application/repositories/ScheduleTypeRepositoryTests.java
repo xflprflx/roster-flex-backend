0d01ee31cd037583ebf1c2044500d3ca967b8bec
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-24 20:29:07 -0300

    saveShouldPersistWithAutoincrementWhenIdIsNull

--- src/test/java/com/rosterflex/application/repositories/ScheduleTypeRepositoryTests.java
+++ src/test/java/com/rosterflex/application/repositories/ScheduleTypeRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.rosterflex.application.repositories;
 
 import com.rosterflex.application.models.ScheduleType;
+import com.rosterflex.application.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,11 +17,12 @@ public class ScheduleTypeRepositoryTests {
     private ScheduleTypeRepository scheduleTypeRepository;
 
     private long existingId;
-
+    private long countTotalScheduleTypes;
 
     @BeforeEach
     void setUp() throws Exception{
         existingId = 1L;
+        countTotalScheduleTypes = 3L;
     }
 
     @Test
@@ -28,5 +30,14 @@ public class ScheduleTypeRepositoryTests {
         scheduleTypeRepository.deleteById(existingId);
         Optional<ScheduleType> result = scheduleTypeRepository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        ScheduleType scheduleType = Factory.createScheduleType();
+        scheduleType.setId(null);
+        scheduleType = scheduleTypeRepository.save(scheduleType);
+        Assertions.assertNotNull(scheduleType.getId());
+        Assertions.assertEquals(countTotalScheduleTypes + 1, scheduleType.getId());
     }
 }

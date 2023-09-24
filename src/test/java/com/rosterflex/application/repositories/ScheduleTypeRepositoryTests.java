f2323bbc00bd572b16a33762429bfbb7a46386d5
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-24 20:18:36 -0300

    adicionando fixture beforeEach

--- src/test/java/com/rosterflex/application/repositories/ScheduleTypeRepositoryTests.java
+++ src/test/java/com/rosterflex/application/repositories/ScheduleTypeRepositoryTests.java
@@ -2,6 +2,7 @@ package com.rosterflex.application.repositories;
 
 import com.rosterflex.application.models.ScheduleType;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,9 +15,16 @@ public class ScheduleTypeRepositoryTests {
     @Autowired
     private ScheduleTypeRepository scheduleTypeRepository;
 
+    private long existingId;
+
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
         scheduleTypeRepository.deleteById(existingId);
         Optional<ScheduleType> result = scheduleTypeRepository.findById(existingId);
         Assertions.assertFalse(result.isPresent());

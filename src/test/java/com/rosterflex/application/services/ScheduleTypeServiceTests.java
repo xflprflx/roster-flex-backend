3f4a51a96901a6e7da5c9e5ef8990d71a25511a3
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-24 23:08:31 -0300

    deleteShouldThrowDatabaseExceptionWhenDependentId

--- src/test/java/com/rosterflex/application/services/ScheduleTypeServiceTests.java
+++ src/test/java/com/rosterflex/application/services/ScheduleTypeServiceTests.java
@@ -1,6 +1,7 @@
 package com.rosterflex.application.services;
 
 import com.rosterflex.application.repositories.ScheduleTypeRepository;
+import com.rosterflex.application.services.exceptions.DatabaseException;
 import com.rosterflex.application.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -25,18 +27,22 @@ public class ScheduleTypeServiceTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
     private long countTotalScheduleTypes;
 
     @BeforeEach
     void setUp() throws Exception{
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 5L;
         countTotalScheduleTypes = 3L;
 
         Mockito.doNothing().when(scheduleTypeRepository).deleteById(existingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(scheduleTypeRepository).deleteById(dependentId);
         Mockito.when(scheduleTypeRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(scheduleTypeRepository.existsById(nonExistingId)).thenReturn(false);
-        //Mockito.when(scheduleTypeRepository.existsById(dependentId)).thenReturn(true);
+        Mockito.when(scheduleTypeRepository.existsById(dependentId)).thenReturn(true);
+
     }
 
     @Test
@@ -52,5 +58,13 @@ public class ScheduleTypeServiceTests {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             scheduleTypeService.delete(nonExistingId);
         });
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId(){
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            scheduleTypeService.delete(dependentId);
+        });
+        Mockito.verify(scheduleTypeRepository, Mockito.times(1)).deleteById(dependentId);
     }
 }

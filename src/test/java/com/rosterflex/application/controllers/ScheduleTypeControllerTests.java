d0059554538e3a3b0e0209c81ed293eb7c6fc8e7
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-26 19:40:50 -0300

    :test_tube: test/findAllShouldReturnPage

---
+++ src/test/java/com/rosterflex/application/controllers/ScheduleTypeControllerTests.java
@@ -0,0 +1,46 @@
+package com.rosterflex.application.controllers;
+
+import com.rosterflex.application.dtos.ScheduleTypeDTO;
+import com.rosterflex.application.services.ScheduleTypeService;
+import com.rosterflex.application.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ScheduleTypeController.class)
+public class ScheduleTypeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ScheduleTypeService scheduleTypeService;
+
+    private ScheduleTypeDTO scheduleTypeDTO;
+    private PageImpl<ScheduleTypeDTO> page;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        scheduleTypeDTO = Factory.createScheduleTypeDTO();
+        page = new PageImpl<>(List.of(scheduleTypeDTO));
+        when(scheduleTypeService.findAllPaged(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        mockMvc.perform(get("/scheduleTypes")).andExpect(status().isOk());
+    }
+
+
+}

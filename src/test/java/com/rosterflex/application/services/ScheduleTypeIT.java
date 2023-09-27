8f74a90dea9a747026d382767ff81ffe5f6cc783
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-26 21:09:08 -0300

    :test_tube: test/findAllPagedShouldReturnPageWhenPage0Size10 and adding @Transactional to the class

--- src/test/java/com/rosterflex/application/services/ScheduleTypeIT.java
+++ src/test/java/com/rosterflex/application/services/ScheduleTypeIT.java
@@ -1,5 +1,6 @@
 package com.rosterflex.application.services;
 
+import com.rosterflex.application.dtos.ScheduleTypeDTO;
 import com.rosterflex.application.repositories.ScheduleTypeRepository;
 import com.rosterflex.application.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,8 +8,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ScheduleTypeIT {
 
     @Autowired
@@ -29,6 +34,16 @@ public class ScheduleTypeIT {
     }
 
     @Test
+    public void findAllPagedShouldReturnPageWhenPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Page<ScheduleTypeDTO> result = scheduleTypeService.findAllPaged(pageRequest);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalScheduleTypes, result.getTotalElements());
+    }
+
+    @Test
     public void deleteShouldDeleteResourceWhenIdExists() {
         scheduleTypeService.delete(existingId);
         Assertions.assertEquals(countTotalScheduleTypes - 1, scheduleTypeRepository.count());
@@ -40,5 +55,4 @@ public class ScheduleTypeIT {
             scheduleTypeService.delete(nonExistingId);
         });
     }
-
 }
